a0041760d6bae0defa93972da99d96c5f51f0d74
utkarsh <vyasutkarsh11@gmail.com>
2024-04-05 12:21:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { Component ,useEffect,useState} from 'react';
+import { render } from 'react-dom';
+import io from 'socket.io-client'
+import Canvas from './pages/Canvas';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
 
 function App() {
+  const [image, setImage] = useState("")
+  const[arr,setarr]=useState([])
+  const[img_count,set_img_count]=useState(-1)
+ 
+  const[room,setroom]=useState("")
+  const socket=io.connect("http://localhost:3001")
+
+  
+  const handleImageChange = async(e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = async() => {
+        setImage(reader.result);
+        const data={
+          room:room,
+          image:image,
+          time:
+            new Date(Date.now()).getHours()+
+            ":" +
+            new Date (Date.now()).getMinutes()
+        }
+        await socket.emit("send_image",data)
+      };
+      reader.readAsDataURL(file);
+      
+     
+      
+    }
+    
+  };
+  useEffect(()=>{
+    
+      socket.on("recieve",(data)=>{
+        setarr((img)=>[...img,data])
+        
+      })
+      set_img_count(img_count+1)
+      
+
+    
+    
+  },[socket])
+  const join=()=>{
+    if(room!==""){
+      socket.emit("join_room",room)
+     
+    }
+    else{
+      console.log("fill the form")
+    }
+    
+  }
+  
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div>
+      <h2>Image Upload</h2>
+      <input type="file" onChange={handleImageChange} />
+      {(
+        <div>
+          <h3>Preview:</h3>
+          {arr.map((e)=>{
+            return <div>
+              {e.time}
+              <img src={e.image} alt="Uploaded" style={{ width: '300px', height: '300px' }} />
+            </div>
+          })}
+        </div>
+      )}
+    </div>
+       <div>
+        Enter the room code
+        <input value={room} onChange={(e)=>{setroom(e.target.value)}}/>
+       </div>
+       <div>
+        <button onClick={join}>Enter </button>
+       </div> */}
+       <Canvas/>
     </div>
   );
 }
+
 
 export default App;
